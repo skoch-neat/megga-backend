c8d9a8a8eb1b9692678b26970b4fd0e35bdc391d
Sarah Koch <sarahkoch810@gmail.com>
2025-02-08 17:52:54 -0800

    Refactor BLS series data structure to use a map for improved readability and maintainability

--- internal/config/bls.go
+++ internal/config/bls.go
@@ -6,14 +6,17 @@ import "os"
 var BLS_API_KEY = os.Getenv("BLS_API_KEY")
 
 // BLS Series IDs
-var BLS_SERIES_IDS = []string{
-	"APU0000708111", // Eggs, grade A, large, per doz.
-	"APU0000702111", // Bread, white, pan, per lb.
-	"APU0000709213", // Milk, fresh, low fat, per gal.
-	"APU0000FF1101", // Chicken breast, boneless, per lb.
-	"APU0000704111", // Bacon, sliced, per lb.
-	"APU0000711111", // Apples, Red Delicious, per lb.
-	"APU0000711311", // Oranges, Navel, per lb.
-	"APU00007471A",  // Gasoline, all types, per gal.
-	"LEU0252881600", // Median usual weekly earnings (CPI-U Adjusted)
+var BLS_SERIES_INFO = map[string]struct {
+	Name string
+	Unit string
+}{
+	"APU0000708111": {"Eggs, Grade A, Large", "per dozen"},
+	"APU0000702111": {"Bread, White, Pan", "per lb."},
+	"APU0000709213": {"Milk, Fresh, Low Fat", "per gallon"},
+	"APU0000FF1101": {"Chicken Breast, Boneless", "per lb."},
+	"APU0000704111": {"Bacon, Sliced", "per lb."},
+	"APU0000711111": {"Apples, Red Delicious", "per lb."},
+	"APU0000711311": {"Oranges, Navel", "per lb."},
+	"APU00007471A": {"Gasoline, All Types", "per gal."},
+	"LEU0252881600": {"Median Usual Weekly Earnings", "constant 1982-1984 dollars"},
 }
